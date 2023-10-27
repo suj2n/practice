02041bfe24136b6c99c0a2b043131811f0da3bec
suj2n <suj2n.k@gmail.com>
2023-10-27 17:39:28 +0900

    api 연습

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,3 @@
-const 정답 = "APPLE";
-
 let attempts = 0;
 let index = 0;
 let timer;
@@ -25,8 +23,10 @@ function appStart() {
     index = 0;
   };
 
-  const handleEnterKey = () => {
+  const handleEnterKey = async() => {
     let 맞은_갯수 = 0;
+    const 응답 = await fetch("/answer");
+    const 정답_객체 = await 응답.json();
 
     for (let i = 0; i < 5; i++) {
       const block = document.querySelector(
